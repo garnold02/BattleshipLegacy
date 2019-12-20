67a22747af4f1ba5ce22d7225c3095db3ee7d182
Garnold02 <gulyas.arnold02@gmail.com>
2019-12-21 00:01:53 +0100

    Improved colors

--- Game/GameObjects/BoardRenderer.cs
+++ Game/GameObjects/BoardRenderer.cs
@@ -1,17 +1,68 @@
-﻿using BattleshipClient.Engine.Rendering;
+﻿using OpenTK;
+using OpenTK.Graphics;
+using BattleshipClient.Engine.Rendering;
+using BattleshipClient.Game.Structure;
+using BattleshipClient.Engine;
 
 namespace BattleshipClient.Game.GameObjects
 {
     class BoardRenderer : GameObject
     {
+        public Board BoardReference;
+
         private MeshRenderer oceanPlaneRenderer;
+        private MeshRenderer smallGridRenderer;
+        private MeshRenderer largeGridRenderer;
+
+        public BoardRenderer(Board boardReference)
+        {
+            BoardReference = boardReference;
+        }
         public override void OnAdded()
         {
-            oceanPlaneRenderer = new MeshRenderer(Assets.Get<Mesh>("plane"), Assets.Get<Shader>("f_lit"))
+            Mesh planeModel = Assets.Get<Mesh>("plane");
+            oceanPlaneRenderer = new MeshRenderer(planeModel, Assets.Get<Shader>("f_lit"))
             {
-                Transform = Transform
+                Material = new Material()
+                {
+                    Opaque = false,
+                    Color = new Color4(0.4f, 0.7f, 0.9f, 0.4f)
+                }
             };
-            Transform.localPosition = new OpenTK.Vector3(0,-0.5f,-2);
+            oceanPlaneRenderer.Transform = new Transform()
+            {
+                localScale = Vector3.One * BoardReference.FullSideLength
+            };
+            smallGridRenderer = new MeshRenderer(planeModel, Assets.Get<Shader>("f_lit"))
+            {
+                Material = new Material()
+                {
+                    Opaque = false,
+                    Color = new Color4(1f, 1f, 1f, 0.02f),
+                    Texture = Assets.Get<Texture>("grid"),
+                    Tiling = new Vector2(BoardReference.FullSideLength, BoardReference.FullSideLength)
+                }
+            };
+            smallGridRenderer.Transform = new Transform()
+            {
+                localPosition = new Vector3(0, 0.01f, 0),
+                localScale = Vector3.One * BoardReference.FullSideLength
+            };
+            largeGridRenderer = new MeshRenderer(planeModel, Assets.Get<Shader>("f_lit"))
+            {
+                Material = new Material()
+                {
+                    Opaque = false,
+                    Color = new Color4(1f, 1f, 1f, 0.02f),
+                    Texture = Assets.Get<Texture>("largeGrid"),
+                    Tiling = new Vector2(BoardReference.SideLength, BoardReference.SideLength)
+                }
+            };
+            largeGridRenderer.Transform = new Transform()
+            {
+                localPosition = new Vector3(0, 0.02f, 0),
+                localScale = Vector3.One * BoardReference.FullSideLength
+            };
         }
         public override void OnRemoved()
         {
@@ -24,6 +75,8 @@ namespace BattleshipClient.Game.GameObjects
         public override void Render()
         {
             oceanPlaneRenderer.Render();
+            smallGridRenderer.Render();
+            largeGridRenderer.Render();
         }
     }
 }
