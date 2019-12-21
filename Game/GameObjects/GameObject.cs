ffd4c66e6d562397d0f81e5c1022afc1112b0514
Garnold02 <gulyas.arnold02@gmail.com>
2019-12-21 11:23:21 +0100

    Added constructor

--- Game/GameObjects/GameObject.cs
+++ Game/GameObjects/GameObject.cs
@@ -1,11 +1,17 @@
 ﻿using BattleshipClient.Engine;
+using BattleshipClient.Game.Structure;
 
 namespace BattleshipClient.Game.GameObjects
 {
     abstract class GameObject
     {
+        public Board Board { get; }
         public Transform Transform { get; set; } = Transform.Identity;
 
+        public GameObject(Board board)
+        {
+            Board = board;
+        }
         public abstract void OnAdded();
         public abstract void OnRemoved();
         public abstract void Update();
