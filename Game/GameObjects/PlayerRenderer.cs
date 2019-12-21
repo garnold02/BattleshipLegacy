77799d565bec8d951a25fba7d104e2d0088500a1
Garnold02 <gulyas.arnold02@gmail.com>
2019-12-21 11:23:34 +0100

    Added player renderer

---
+++ Game/GameObjects/PlayerRenderer.cs
@@ -0,0 +1,43 @@
+﻿using BattleshipClient.Game.Structure;
+using System.Collections.Generic;
+
+namespace BattleshipClient.Game.GameObjects
+{
+    class PlayerRenderer : GameObject
+    {
+        public Player Player { get; }
+
+        private List<ShipRenderer> shipRenderers;
+
+        public PlayerRenderer(Board board, Player player) : base(board)
+        {
+            Player = player;
+            shipRenderers = new List<ShipRenderer>();
+        }
+        public override void OnAdded()
+        {
+
+        }
+        public override void OnRemoved()
+        {
+
+        }
+        public override void Render()
+        {
+            foreach (ShipRenderer shipRenderer in shipRenderers)
+            {
+                shipRenderer.Render();
+            }
+        }
+        public override void Update()
+        {
+
+        }
+
+        public void CreateShipRenderer(Ship ship)
+        {
+            ShipRenderer shipRenderer = new ShipRenderer(Board);
+            shipRenderer.SetProperties(ship);
+        }
+    }
+}
