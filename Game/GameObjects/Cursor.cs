37035d21ad7f6e8ef25bcd450b62cfbd935d0dea
Garnold02 <gulyas.arnold02@gmail.com>
2019-12-21 11:22:52 +0100

    Added cursor gameobject

---
+++ Game/GameObjects/Cursor.cs
@@ -0,0 +1,34 @@
+﻿using BattleshipClient.Engine.Rendering;
+using BattleshipClient.Game.Structure;
+
+namespace BattleshipClient.Game.GameObjects
+{
+    class Cursor : GameObject
+    {
+        private MeshRenderer claimRenderer;
+        private ShipRenderer shipRenderer;
+        private MeshRenderer attackRenderer;
+
+        public Cursor(Board board) : base(board)
+        {
+
+        }
+        public override void OnAdded()
+        {
+            claimRenderer = new MeshRenderer(Assets.Get<Mesh>("plane"), Assets.Get<Shader>("f_lit"));
+            shipRenderer = new ShipRenderer(Board);
+        }
+        public override void OnRemoved()
+        {
+
+        }
+        public override void Update()
+        {
+
+        }
+        public override void Render()
+        {
+
+        }
+    }
+}
